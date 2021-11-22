a695bfe3f8afe801f08856c25533f9efb88106af
Tudorandrei <tudorandrei.dumitrasc@gmail.com>
2021-11-22 23:17:10 +0200

    update

---
+++ trainer.py
@@ -0,0 +1,15 @@
+import pytorch_lightning as pl
+
+
+class LitModel(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(28 * 28, 128)
+        self.layer_2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.layer_1(x)
+        x = F.relu(x)
+        x = self.layer_2(x)
+        return x
